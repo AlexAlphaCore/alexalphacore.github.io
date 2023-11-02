61fe8577af510beaf257f09372c6587df87c5532
Alex Lorenzo <138170081+AlexAlphaCore@users.noreply.github.com>
2023-11-02 12:53:07 -0300

    CSS Fixes and Support keyboard virtual

--- java.js
+++ java.js
@@ -10,3 +10,37 @@ function copyToClipboard() {
         alert('No se pudo copiar al portapapeles. Puede que tu navegador no sea compatible.');
     }
 }
+
+document.body.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        calculate(); // Si se presiona la tecla "Enter", calcula el resultado
+    } else if (event.key === 'c' || event.key === 'C') {
+        clearDisplay(); // Si se presiona la tecla "C" o "c", borra la pantalla
+    } else if (event.key === 'Escape') {
+        clearDisplay(); // Si se presiona la tecla "Escape", borra la pantalla
+    }  else if (event.key === 'Backspace') {
+        // Si se presiona "Backspace", elimina el último carácter de la pantalla
+        var display = document.getElementById('display');
+        display.value = display.value.slice(0, -1);  
+    } else if (/[0-9\+\-\*\/]/.test(event.key)) {
+        addToDisplay(event.key); // Si se presiona un número o un operador, agrégalo a la pantalla
+    }
+});
+
+// Agregar eventos táctiles para los botones
+var buttons = document.querySelectorAll('.calculator button');
+buttons.forEach(function (button) {
+    button.addEventListener('touchstart', function () {
+        // Lógica para manejar el evento táctil
+        var buttonValue = this.textContent;
+        if (/[0-9\+\-\*\/]/.test(buttonValue)) {
+            addToDisplay(buttonValue);
+        } else if (buttonValue === '=') {
+            calculate();
+        } else if (buttonValue === 'C') {
+            clearDisplay();
+        } else if (buttonValue === 'Copiar') {
+            copyToClipboard();
+        }
+    });
+});
